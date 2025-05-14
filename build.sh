7c87254fe41ed7a22c04974f6ed1e5b7054a3245
Ron Rise <ron@siteworxpro.com>
2025-05-13 22:24:31 -0400

    oops - thought I got that one.

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ for distro in $(go tool dist list)
 do
     arrIN=(${distro//\// })
 
-    if [[ ${arrIN[0]} == 'linux' || ${arrIN[0]} == 'darwin'  || ${arrIN[0]} == 'freebsd' || ${arrIN[0]} == 'windows' ]]; then
+    if [[ ${arrIN[0]} == 'linux' || ${arrIN[0]} == 'darwin' ]]; then
       echo "Building $distro..."
       GOOS=${arrIN[0]} GOARCH=${arrIN[1]} go build --ldflags="-X 'github.com/siteworxpro/rsa-file-encryption/printer.Version=$(git describe --tags --abbrev=0)'" -o dist/rsa-file-encryption_${arrIN[0]}_${arrIN[1]}
+      gpg --detach-sign "dist/rsa-file-encryption_${arrIN[0]}_${arrIN[1]}"
     fi
 done
